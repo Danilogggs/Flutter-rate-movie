0ad130b17339b980a8f5051284cb1120173a2795
Danilogggs <dangggs@gmail.com>
2025-11-08 16:50:23 -0300

    v2, ajustes com erros

--- rate_my_movie_flutter/lib/app.dart
+++ rate_my_movie_flutter/lib/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart';
 
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
--- rate_my_movie_flutter/lib/screens/search_screen.dart
+++ rate_my_movie_flutter/lib/screens/search_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/movies_provider.dart';
-import '../providers/auth_provider.dart';
 import '../widgets/movie_card.dart';
 
 class SearchScreen extends StatefulWidget {
